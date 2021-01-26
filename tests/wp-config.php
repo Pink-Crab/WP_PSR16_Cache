84cf38d5fa62e3ada13e97d01a70d04299f2cea0
gin0115 <glynn.quelch@gmail.com>
2021-01-26 14:32:27 +0000

    House Keeping

--- tests/wp-config.php
+++ tests/wp-config.php
@@ -33,7 +33,7 @@ define( 'WP_DEBUG', true );
 define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ?: 'pc_core_tests' );
 define( 'DB_USER', getenv( 'WP_DB_USER' ) ?: 'root' );
 define( 'DB_PASSWORD', getenv( 'WP_DB_PASS' ) ?: '' );
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', '127.0.0.1:3306' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
